96c061183df04b6e2a0e3b7cda2e59f8d897afe8
unknown <ashkin@ASHKELON.softservecom.com>
2013-08-01 19:02:05 +0300

    First version of the second task

---
+++ Task2SmartPTR/Task2SmartPTR/SHSmartPtr.h
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <conio.h>
+template <typename TYPE>
+class SHSmartPtr
+{
+public:
+	SHSmartPtr() // default constructor
+	{
+		storedObject = NULL;
+		referenceCounter = 1;
+	}
+	SHSmartPtr(TYPE *pointerToStore) //Constructor for wrap something in smart poiter
+	{
+		storedObject = pointerToStore;
+		referenceCounter = 1;
+	}
+	~SHSmartPtr(); //destructor for clean the memory
+	inline TYPE* operator -> () { return  storedObject; };
+	inline TYPE& operator * ()  { return *storedObject; };
+	inline void setUpData(TYPE objectValue)
+	{
+		if(storedObject == NULL)
+		{
+			storedObject = new TYPE;
+		}
+		*storedObject = objectValue;
+	}
+	//Copy constructors
+	inline SHSmartPtr(const SHSmartPtr &anotherSmartPtr);
+	inline operator bool()const { return storedObject != NULL; }
+	//Assignment operator
+	SHSmartPtr<TYPE>& operator= (const SHSmartPtr<TYPE>& anotherSmartPtr);
+private:
+	TYPE *storedObject; //Pointer to object wrapped in smart pointer
+    mutable unsigned int referenceCounter; // increments in copy constructor and assignment operator, decrements in destructor
+};
+
+template <typename TYPE>
+SHSmartPtr<TYPE>::~SHSmartPtr()
+{	
+	TYPE temporaryStorage;
+	if(storedObject != NULL)
+	{
+		temporaryStorage = *storedObject;
+	}
+	--referenceCounter; //Decrements referenceCount
+	if(referenceCounter == 0) //And if it equals zero
+	{
+		cout << "Now reference count in object "<<temporaryStorage<<" is zero. Object'll be destroyed" <<endl;	
+		if(storedObject!= NULL){
+			delete storedObject; //destroy the object
+			
+		}
+		_getch(); // For checking if all objects deleted
+	}
+	
+}
+
+template <typename TYPE>
+SHSmartPtr<TYPE>::SHSmartPtr(const SHSmartPtr &anotherSmartPtr)
+{
+	storedObject = anotherSmartPtr.storedObject;
+	referenceCounter = anotherSmartPtr.referenceCounter;
+	++referenceCounter; //Copy constructor must increment reference counter
+}
+
+template <typename TYPE>
+SHSmartPtr<TYPE>& SHSmartPtr<TYPE>::operator=(const SHSmartPtr &anotherSmartPtr)
+{
+	if(referenceCounter == 1) // If it's the last referens to some object
+	{
+		SHSmartPtr<TYPE> temporary = *this; //delete this object
+		--temporary.referenceCounter; 
+	} 
+	else //Elese decrement reference counter
+	{
+		--referenceCounter;
+	}
+	//change the data
+	storedObject = anotherSmartPtr.storedObject;
+	referenceCounter = anotherSmartPtr.referenceCounter;
+	++referenceCounter; //and increment reference counter
+	return *this;
+
+}
---
+++ Task2SmartPTR/Task2SmartPTR/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <conio.h>
+#include "SHSmartPtr.h"
+#include <string>
+using namespace std;
+void show(SHSmartPtr<string> obj);
+int main(int argc, char **argv)
+{
+	//create two objects wrapped by smart pointers
+	SHSmartPtr<string> ptr(new string("first")); 
+	cout << *ptr << endl;
+	SHSmartPtr<string> ptr2(new string("second"));
+	
+	// some block for checking if temporary objects deletes corectly
+	if (true)
+	{
+		SHSmartPtr<string> anohterPtr = ptr; //Creating smart pointer inside the block (using copy constructor)
+		cout << *anohterPtr << endl;
+		anohterPtr = ptr2; //Try assignment operator
+		show(anohterPtr); //Pass smart pointer into the function by value, for check if temporarty objects deletes corectly
+		SHSmartPtr<string> ptr3(new string("third"));  //creating new smart poiner inside the block
+		ptr3 = anohterPtr; //another test of assignment operator
+	}
+	_getch();
+	return 0;
+}
+void show(SHSmartPtr<string> obj)
+{
+	cout << *obj << endl;
+}
